525ae73e1c172d8c712bcc656e6e777fdfe5e99a
FicLel <victorleal190@gmail.com>
2016-12-28 18:07:36 -0600

    Agregando diseño

---
+++ front-end/Login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Escuela Salesiana María Mazzarello</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	</head>
+	<body style="background-color:#FB65E4;">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-12 col-lg-12">
+					<br/>
+					<br/>
+					<br/>
+					<div class="jumbotron">
+						<h1>BIENVENIDAS/OS</h1>
+						<p class="text-xs-justify">Usted esta alojado en el panel de inicio de sesión del sistema de notas de la 
+							Escuela María Mazzarello.<br/> Para hacer uso del sistema solo debe ingresar sus datos
+							correctamente, no importa si usted es docente de la institución o Alumna.</p>
+						<form>
+							<div class="form-group">
+								<label>Ingrese su usuario</label>
+								<input id="usuario" class="form-control">
+							</div>
+							<div class="form-group">
+								<label>Ingrese su contraseña</label>
+								<input type="password" id="password" class="form-control">
+							</div>
+							<input type="submit" class="btn btn-info btn-block" value="Ingresar">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<script type="text/javascript" src ="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</body>
+</html>
